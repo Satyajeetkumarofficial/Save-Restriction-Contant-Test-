ba2c04f44b409346ff753dc2dedaf2f6b867c2b3
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-17 13:20:29 +0530

    Add files via upload

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -209,7 +209,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
         
         try:
             await client.send_document(chat, file, thumb=ph_path, caption=caption, reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML, progress=progress, progress_args=[message,"up"])
-            await client.send_document(-1002514022613, file, caption=caption)
+            await client.send_document(-1002514022613, file, caption=caption, thumb=thumbnail)
         except Exception as e:
             if ERROR_MESSAGE == True:
                 await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
@@ -224,7 +224,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
         
         try:
             await client.send_video(chat, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=ph_path, caption=caption, reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML, progress=progress, progress_args=[message,"up"])
-            await client.send_video(-1002514022613, file, caption=caption)
+            await client.send_video(-1002514022613, file, caption=caption, thumb=thumbnail)
         except Exception as e:
             if ERROR_MESSAGE == True:
                 await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
@@ -233,7 +233,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
     elif "Animation" == msg_type:
         try:
             await client.send_animation(chat, file, reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
-            await client.send_animation(-1002514022613, file, caption=caption)
+            await client.send_animation(-1002514022613, file, caption=caption, thumb=thumbnail)
         except Exception as e:
             if ERROR_MESSAGE == True:
                 await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
@@ -260,7 +260,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 
         try:
             await client.send_audio(chat, file, thumb=ph_path, caption=caption, reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML, progress=progress, progress_args=[message,"up"])   
-            await client.send_audio(-1002514022613, file, caption=caption)
+            await client.send_audio(-1002514022613, file, caption=caption, thumb=thumbnail)
         except Exception as e:
             if ERROR_MESSAGE == True:
                 await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
@@ -270,7 +270,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
     elif "Photo" == msg_type:
         try:
             await client.send_photo(chat, file, caption=caption, reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
-            await client.send_photo(-1002514022613, file, caption=caption)
+            await client.send_photo(-1002514022613, file, caption=caption, thumb=thumbnail)
         except:
             if ERROR_MESSAGE == True:
                 await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML)
@@ -331,3 +331,5 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
     except:
         pass
         
+if thumbnail:
+    os.remove(thumbnail)
